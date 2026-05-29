ffc63ef87c66fa84cdfc79168e0b5b4d4b4479f3
Pranjal Gajaya <gajayapranjal@gmail.com>
2026-05-29 17:10:52 +0530

    Day 1 uploaded

---
+++ Day 1/02.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int num, product=1;
+    printf("Enter a Number");
+    scanf("%d", &num);
+
+    for(int i = 1; i <=10; i++){
+        product = num * i;
+        printf("%d x %d = %d\n",num,i,product);
+    }
+
+    return 0;
+}
