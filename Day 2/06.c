613f023628f6816294390651fb2515570dd19027
Pranjal Gajaya <gajayapranjal@gmail.com>
2026-05-31 14:47:16 +0530

    Uploaded Day 2

---
+++ Day 2/06.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int num, temp, rev=0;
+    printf("Enter a Number ");
+    scanf("%d", &num);
+    
+    while(num != 0){
+        temp = num % 10;
+        rev = rev*10 + temp;
+        num = num /10;
+    }
+
+    printf("Reverse of your number is %d", rev);
+
+    return 0;
+}
