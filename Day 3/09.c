8cd8d8a88c0b7d9fb964afe6199b9aac8f6899db
Pranjal Gajaya <gajayapranjal@gmail.com>
2026-05-31 14:44:56 +0530

    Day 3 Uploaded

---
+++ Day 3/09.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+    int i, count=0, num;
+    printf("Enter a Number ");
+    scanf("%d", &num);
+
+    for(i = 2; i < num; i++){
+        if(num % i == 0){
+            count++;
+            break;
+        }
+    }
+
+    if (count == 0)
+    {
+        printf("Number is Prime");
+    }
+    else{
+        printf("Number is NOT Prime");
+    }
+    
+    return 0;
+}
