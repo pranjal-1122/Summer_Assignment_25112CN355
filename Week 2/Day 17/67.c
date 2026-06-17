2c9a616e3d4cc42aade7831e87e6a4e70f9ea22b
Pranjal Gajaya <gajayapranjal@gmail.com>
2026-06-17 21:37:40 +0530

    Day 17 Uploaded

---
+++ Week 2/Day 17/67.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+    int n1, n2, i, j;
+    printf("enter size of first array: ");
+    scanf("%d", &n1);
+    int arr1[n1];
+    printf("enter %d elements:\n", n1);
+    for (i = 0; i < n1; i++){
+        scanf("%d", &arr1[i]);
+    }
+
+    printf("enter size of second array: ");
+    scanf("%d", &n2);
+    int arr2[n2];
+    printf("Enter %d elements:\n", n2);
+     for (i = 0; i < n2; i++){ 
+        scanf("%d", &arr2[i]);
+    }
+
+    printf("Intersection: ");
+    for (i = 0; i < n1; i++) {
+        for (j = 0; j < n2; j++) {
+            if (arr1[i] == arr2[j]) {
+                printf("%d ", arr1[i]);
+                break;
+            }
+        }
+    }
+    return 0;
+}
